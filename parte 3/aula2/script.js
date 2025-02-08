b1446724addc26ff3c8fcb4a695737b881025d45
GersonESS <gebhsantos@gmail.com>
2025-02-07 22:05:23 -0300

    a

--- parte 3/aula2/script.js
+++ parte 3/aula2/script.js
@@ -1,21 +1,21 @@
 function adicionarTarefa() {
 
   //recebe valor do input do usuário
-  let inputTarefa = document.getElementById("inputTarefa")
+  const inputTarefa = document.getElementById("inputTarefa")
   let tarefa = inputTarefa.value.trim()
-  
+  const mensagem = document.getElementById("mensagem")
   if (tarefa === "") 
     {
       //mensagem de tarefa adicionada com erro
     let mensagemErro = ("Por favor, insira uma tarefa válida!") 
-    document.getElementById("mensagem").textContent = mensagemErro
+    mensagem.textContent = mensagemErro
 
     }else{  
       //mensagem de tarefa adicionada com sucesso
-      let mensagem = "Tarefa adicionada com sucesso!"
-      document.getElementById("mensagem").textContent = mensagem
+      let mensagemSussesso = "Tarefa adicionada com sucesso!"
+      mensagem.textContent = mensagemSussesso
       //cria novo item (li) e insere na (lista ul)
-      let listaTarefas = document.getElementById("listaTarefas")
+      const listaTarefas = document.getElementById("listaTarefas")
       let novaTarefa = document.createElement("li")
       novaTarefa.textContent = tarefa
       listaTarefas.appendChild(novaTarefa)
