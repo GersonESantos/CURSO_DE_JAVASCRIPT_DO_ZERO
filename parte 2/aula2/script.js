f6bc29dbd8dfa06983ae0a0a6105520f61a79777
GersonESS <gebhsantos@gmail.com>
2025-02-26 19:45:09 -0300

    c

--- parte 2/aula2/script.js
+++ parte 2/aula2/script.js
@@ -3,5 +3,6 @@ function adicionarTarefa() {
   let inputTarefa = document.getElementById("inputTarefa");
   let tarefa= inputTarefa.value;
   document.getElementById("mensagem").textContent = tarefa;
+  // document.getElementById("mensagem").textContent =document.getElementById("inputTarefa").value;
 
       }
