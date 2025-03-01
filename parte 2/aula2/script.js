f837b41594857593f0340ea510699181a7a8e0f6
GersonESS <gebhsantos@gmail.com>
2025-03-01 19:05:41 -0300

    a

--- parte 2/aula2/script.js
+++ parte 2/aula2/script.js
@@ -2,7 +2,6 @@ function adicionarTarefa() {
   let mensagem = "Tarefa adicionada com sucesso!";
   let inputTarefa = document.getElementById("inputTarefa");
   let tarefa= inputTarefa.value;
-  document.getElementById("mensagem").textContent = tarefa;
-  // document.getElementById("mensagem").textContent =document.getElementById("inputTarefa").value;
-
-      }
+  // console.log(tarefa)// document.getElementById("mensagem").textContent =document.getElementById("inputTarefa").value;
+  // document.getElementById("mensagem").textContent = tarefa;
+     }
